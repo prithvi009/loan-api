44b50b357a71f25d972ccfcb10f7e3652db4ec64
Prithviraj Awatade <96919987+prithvi009@users.noreply.github.com>
2024-04-03 05:10:59 +0530

    sequelize setup

---
+++ src/config/sequelize.ts
@@ -0,0 +1,12 @@
+
+
+import {Sequelize} from 'sequelize'
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+// Option 1: Passing a connection URI
+export const sequelize = new Sequelize(`${process.env.DATABASE_URL}`,{
+    logging: false,
+}) // Example for postgres
+
